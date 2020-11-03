53ef39c3a59f0366fe7a1f3abb44deb34058b1eb
Whit <may.whitney21@gmail.com>
2020-11-02 17:06:25 -0700

    Create AccountController.cs

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,11 @@
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+}
+}
